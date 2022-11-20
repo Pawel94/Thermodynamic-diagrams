cdbded5e695f86fa6bb1b1a9ef5a2c7462482722
paszewcz <pawel.szewczak@capgemini.com>
2022-11-20 12:44:55 +0100

    [2] - Added first commit with diagram

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
-
+import * as Highcharts from 'highcharts';
+const Draggable = require("highcharts/modules/draggable-points.js");
+Draggable(Highcharts);
+const Exporting = require("highcharts/modules/exporting.js");
+Exporting(Highcharts);
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'chart';
+  Highcharts: typeof Highcharts = Highcharts;
+  linechart: any = {
+    series: [
+      {
+        data: [1, 2, 3],
+      },
+    ],
+    plotOptions: {
+      series: {
+        stickyTracking: false,
+        dragDrop: {
+          draggableY: false,
+          draggableX:true
+        }
+      },
+      column: {
+        stacking: "normal",
+        minPointLength: 2
+      },
+      line: {
+        cursor: "ns-resize"
+      }
+    },
+    chart: {
+      type: 'line',
+    },
+    title: {
+      text: 'linechart',
+    },
+  };
 }
